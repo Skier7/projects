e8f8d30c7a8161794022dc4ea0e0478a96037a27
Skier7 <157335238+Skier7@users.noreply.github.com>
2026-05-20 09:12:53 -0600

    Fixt spelling of my Ski program.

--- ski.py
+++ ski.py
@@ -1,13 +1,13 @@
-#this is a dicshinary
+#This is a dicshinary
 prices={"s": 1000, "b": 250, "h": 500, "g": 10, "p": 400}
 total=0
 while True:
-    options=input("wellcome to my ski shop, what do you want? s for a pair of skis, b for a pair of boots, h for helmit, g for gluvs, p for poles and done to finish")
+    options=input("Welcome to my ski shop, what do you want? S for a pair of skis, B for a pair of boots, H for helmet, G for gluvs, P for poles and done to finish")
     if options == "done":
-        print(f"thankyou for shopping, your total is ${total}")
+        print(f"Thank you for shopping, your total is ${total}")
         quit()
     if options in prices:
         q=int(input("how many would you like?"))
         total+=prices[options]*q
     else:
-        print("sorry but we don't have that, try again or get out!")
+        print("Sorry but we don't have that, try again")
