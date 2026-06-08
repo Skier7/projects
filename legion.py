9133575db21256104a2b6c08111ed5e91d9aa043
Skier7 <157335238+Skier7@users.noreply.github.com>
2026-06-07 21:41:33 -0600

    Updated the legion commandmints, they are the rules to live buy.

--- legion.py
+++ legion.py
@@ -1,7 +1,7 @@
-Legion=["1. assume nothing", "2. how you do anything is how you do everything", "3. Master your surroundings", "4. Never make it personal", "5. if you don't know what to do, do nothing", "6. Question orders", "7. One mission at a time", "8. Never kill a kid", "9. Always play offense", "10. Never let an innocent die", "11. Only do things with a point"]
+Legion=["1. Assume nothing", "2. How you do anything is how you do everything", "3. Master your surroundings", "4. Never make it personal", "5. If you don't know what to do, do nothing", "6. Question orders", "7. One mission at a time", "8. Never kill a kid", "9. Always play offense", "10. Never let an innocent die", "11. Only do things with a point", "12. Use your resources"]
 print("Hello, the commands are rules to live by")
 while True:
-    cmd=input("choose a number between 1 and 11, all to list all commands or q to quit")
+    cmd=input("choose a number between 1 and 12, all to list all commands or q to quit")
     if cmd == "1":
         print(Legion[0])
     elif cmd == "2":
@@ -22,8 +22,10 @@ while True:
         print(Legion[8])
     elif cmd == "10":
         print(Legion[9])
-    elif cmd== "11":
+    elif cmd == "11":
         print(Legion[10])
+    elif cmd == "12":
+        print(Legion[11])
     elif cmd == "all":
         print("all legion commands are," ,Legion)
     elif cmd == "Q" or cmd =="q":
