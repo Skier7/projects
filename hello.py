aa69de69ced2ebf4b025b7152bcc7ea37cc7fbad
Skier7 <157335238+Skier7@users.noreply.github.com>
2026-06-24 17:12:58 -0600

    Added the int function to the age prompt.

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
-name=input("enter your   name")
-while name == "":
-    print("you didn't enter a name, you must enter a name to continue")
-    name=input("enter your   name")
-print(f"hello {name}")
-age=input("enter your age")
-print(f"your age is{age}")
-color=("green")
+name = input("enter your name: ").strip()
+while not name:
+    print("You didn't enter a name; please enter a name to continue.")
+    name = input("enter your name: ")
+print(f"hello {name}!")
+age=int(input("enter your age"))
+print(f"your age is {age}")
+color = "green"
 print(f"your favorite color is {color}")
-sport=("skiing")
-drink=("hot chocklit")
+sport = "skiing"
+drink = "hot chocolate"
 print(f"I love {sport} and I like to have {drink} when I come in for a warmup")
