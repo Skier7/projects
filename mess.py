3a40f1a1e32ecc609a9413ccdffa5869712ca889
Skier7 <157335238+Skier7@users.noreply.github.com>
2026-07-22 21:07:57 -0600

    Fixed a spelling mistake.

--- mess.py
+++ mess.py
@@ -1,2 +1,2 @@
 print("This is a mess of a program. Come on, what were you expecting?")
-print("Did you really thing this was gonna be a serious program?")
+print("Did you really think this was gonna be a serious program?")
